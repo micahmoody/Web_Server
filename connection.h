a8c6fdbd4724d91d5accad7268fde0035a326a9a
Micah M <717mlm@gmail.com>
2026-09-07 18:28:36 -0400

    added disconnect()

--- connection.h
+++ connection.h
@@ -1,12 +1,21 @@
+#include <stdio.h>
+
 #ifndef CONNECTION_H
 #define CONNECTION_H
 
 struct connection {
     int fd;
+
     char *read_buf;
     char *write_buf;
-    int wp;
+
+    int rs;
+    int ws;
+
     int rp;
+    int wp;
 };
+
+void disconnect(int epfd, struct connection *con);
 
 #endif
