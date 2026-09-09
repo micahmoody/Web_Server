d88b2df2866397e2cc041ea88670b6095756e75b
Micah M <717mlm@gmail.com>
2026-09-08 20:48:59 -0400

    added MAX_HEADER_COUNT

--- constants.h
+++ constants.h
@@ -1,13 +1,19 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+//socket
 #define PORT 8080
 #define BACKLOG 10
 
+//epoll
 #define EPOLL_READY_SIZE 64
 
+//read buffer
 #define INITIAL_READ_BUFFER_SIZE 4096
 #define MAX_READ_BUFFER_SIZE 65536
 #define READ_BUFFER_RESIZE_FACTOR 2
+
+//http
+#define MAX_HEADER_COUNT 12
 
 #endif
